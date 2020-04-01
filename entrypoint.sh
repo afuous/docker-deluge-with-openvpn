1ff0ceae16fe02ef748247436bcb36f3e64df49f
afuous <ben@bheller.me>
2020-04-01 00:30:46 -0700

    Add missing openvpn argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ gid=$3
 groupadd --gid $gid $user
 useradd -m --uid $uid --gid $gid $user
 
-openvpn --daemon --ping-restart --config /etc/openvpn/config.ovpn
+openvpn --daemon --ping-restart 60 --config /etc/openvpn/config.ovpn
 su $user -c deluged
 su $user -c deluge-web
