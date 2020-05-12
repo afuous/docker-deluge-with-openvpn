71d8804856f359d74adc00ec48c197dfde49a837
afuous <ben@bheller.me>
2020-05-12 13:01:43 -0700

    Ensure openvpn successfully connects
    For some reason openvpn often hangs when connecting, and the last line
    of output always includes "UDP link remote". So this just loops and
    restarts openvpn until it doesn't hang.

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ RUN apt-get update
 RUN apt-get upgrade
 
 RUN apt-get install -y deluged deluge-web openvpn
+RUN apt-get install -y curl procps
 
 COPY entrypoint.sh /root/entrypoint.sh
 ENTRYPOINT ["/root/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,16 @@ gid=$3
 groupadd --gid $gid $user
 useradd -m --uid $uid --gid $gid $user
 
-openvpn --daemon --ping-restart 60 --config /etc/openvpn/config.ovpn
+while true; do
+	openvpn --config /etc/openvpn/config.ovpn > /tmp/openvpnoutput &
+	openvpnpid=$!
+	sleep 2
+	if [ "$(cat /tmp/openvpnoutput | tail -n 1 | grep 'UDP link remote')" ]; then
+		kill $openvpnpid
+	else
+		break
+	fi
+done
+
 su $user -c deluged
 su $user -c deluge-web
