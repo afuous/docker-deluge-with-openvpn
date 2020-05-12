7a24e431c964c4c80c1a4a2e8c6fdfc40f7311b0
afuous <ben@bheller.me>
2020-05-12 13:26:54 -0700

    Check for vpn connection with curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,20 @@ gid=$3
 groupadd --gid $gid $user
 useradd -m --uid $uid --gid $gid $user
 
+startip="$(curl https://api.ipify.org/)"
 while true; do
 	openvpn --config /etc/openvpn/config.ovpn > /tmp/openvpnoutput &
 	openvpnpid=$!
 	sleep 2
-	if [ "$(cat /tmp/openvpnoutput | tail -n 1 | grep 'UDP link remote')" ]; then
-		kill $openvpnpid
-	else
-		break
+	if [ ! "$(cat /tmp/openvpnoutput | tail -n 1 | grep 'UDP link remote')" ]; then
+		sleep 10
+		newip="$(curl https://api.ipify.org/)"
+		curlexitcode=$?
+		if [ $curlexitcode == 0 ] && [ "$newip" != "$startip" ]; then
+			break
+		fi
 	fi
+	kill $openvpnpid
 done
 
 su $user -c deluged
